4cf86f43a4f708f586342182e982c03e5eb8dac0
Scarlatt Luz <contatoscarlatt@gmail.com>
2020-12-10 17:29:35 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API =
+  'https://api.hgbrasil.com/weather?woeid=455827&format=json-cors&locale=pt';
+
+export default class App extends React.Component {
+  state = {
+    city: '',
+    forecast: [],
+  };
+
+  componentDidMount() {
+    axios.get(API).then(({ data }) => {
+      this.setState({
+        city: data.results.city_name,
+        forecast: data.results.forecast,
+      });
+    });
+
+    /*fetch(API)
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          city: json.results.city_name,
+        });
+      }); */
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.state.city}</h1>
+        <table className="striped centered">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Min.</th>
+              <th>Max.</th>
+              <th>Previsão</th>
+              <th>Img</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.forecast.map((day) => {
+              return (
+                <tr key={day.date}>
+                  <td>{day.date}</td>
+                  <td>{day.min}</td>
+                  <td>{day.max}</td>
+                  <td>{day.description}</td>
+                  <td>
+                    <img
+                      src={`/weather-icons/weather-icons/${day.condition}.svg`}
+                      alt={day.description}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
